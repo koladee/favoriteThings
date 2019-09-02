32455eb96c211a3826674feaade72ba6f07dca5e
Kolade Emmanuel <48717622+koladee@users.noreply.github.com>
2019-09-02 13:26:34 +0100

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -5,9 +5,8 @@ def quiz():
     key = 'TluxwB3fV_GWuLkR1_BzGs1Zk90TYAuhNMZP_0q4WyM='
 
     # Oh no! The code is going over the edge! What are you going to do?
-    message = b'gAAAAABc-TORPfaj_2JPeQWNgNrHfxvio8ZaINBFH6NwdEsSfan12qHgpn9GBXhuQSQBHQso35N-qfnYbyDgg6S7BWZqZ-93' \
-              b'JBnnWgVM4FOHEFcneVhTjc3LTvf3ksM-EthQb2f8uT3hLKihjVEkC53a9j4lZztI3qMBMT4mx17CsRpM6AhAqh0='
-
+    message = b'gAAAAABdbQaJKitS3SO7uy4miEiRhnn6zegMWdZYEOJu2gNnIhC-jdZLjjjUMbqIUYNHFcs2jZzRsIHcxqNm9t5eAP'/
+              b'XrosEXS0kJ5Wdt60Q3TBeIh1MECdBJfjCVvBbpEemrNKqzG3TzdhC_ZaNKaQ__MXGpinpotlcUATFPxtl2tAMUZxaJtlM='
     f = Fernet(key)
     r = f.decrypt(message)
     print(r)
